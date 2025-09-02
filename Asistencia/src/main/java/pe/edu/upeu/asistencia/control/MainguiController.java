70cdaa9fac9909b734fbef36f9b82b62a6e76661
dmamanipar <dmamanipar@unsa.edu.pe>
2025-09-02 09:58:05 -0500

    Avance P2.1

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -1,10 +1,15 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import org.springframework.stereotype.Controller;
+
+
 
 @Controller
 public class MainguiController {
@@ -15,6 +20,33 @@ public class MainguiController {
     MenuBar menuBar;
     @FXML
     TabPane tabPane;
+    @FXML
+    MenuItem menuItem1, menuItemC;
 
+    @FXML
+    public void initialize(){
+
+    }
+
+    class MenuItemListener{
+        public void handle(ActionEvent e){
+
+        }
+        private void abrirTabPaneFXML(String fxmlPath, String tittle){
+            try {
+
+            }catch (Exception ex){
+                
+            }
+        }
+
+
+    }
+
+    class MenuListener{
+        public void handle(Event e){
+
+        }
+    }
 
 }

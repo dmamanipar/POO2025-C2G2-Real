d705b63b72a3d46280e9b4c122dd26995f40d560
dmamanipar <dmamanipar@unsa.edu.pe>
2025-09-08 09:27:20 -0500

    Lista datos en table view

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteRepositorio.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteRepositorio.java
@@ -2,8 +2,8 @@ package pe.edu.upeu.asistencia.repositorio;
 
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
-import pe.edu.upeu.asistencia.enums.CARRERA;
-import pe.edu.upeu.asistencia.enums.TIPO_PARTICIPANTE;
+import pe.edu.upeu.asistencia.enums.Carrera;
+import pe.edu.upeu.asistencia.enums.TipoParticipante;
 import pe.edu.upeu.asistencia.modelo.Participante;
 
 import java.util.ArrayList;
@@ -18,8 +18,8 @@ public abstract class ParticipanteRepositorio {
                       new SimpleStringProperty("43631917"),
                       new SimpleStringProperty("Juan"),
                       new SimpleStringProperty("Apaza"),
-                      new SimpleBooleanProperty(true), CARRERA.Sistemas,
-                      TIPO_PARTICIPANTE.Asistente
+                      new SimpleBooleanProperty(true), Carrera.SISTEMAS,
+                      TipoParticipante.ASISTENTE
               )
       );
       listaParticipantes.add(
@@ -27,8 +27,8 @@ public abstract class ParticipanteRepositorio {
                       new SimpleStringProperty("43631918"),
                       new SimpleStringProperty("Pedro"),
                       new SimpleStringProperty("Gutierrez"),
-                      new SimpleBooleanProperty(true), CARRERA.Sistemas,
-                      TIPO_PARTICIPANTE.Asistente
+                      new SimpleBooleanProperty(true), Carrera.SISTEMAS,
+                      TipoParticipante.ASISTENTE
               )
       );
       return listaParticipantes;

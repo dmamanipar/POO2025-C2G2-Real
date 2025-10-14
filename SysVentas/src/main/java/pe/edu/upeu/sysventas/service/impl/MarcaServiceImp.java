10b9cf2505e3b67cac5153062a9c45266bbd55c8
dmamanipar <dmamanipar@unsa.edu.pe>
2025-10-14 11:17:32 -0500

    Servicio y Repositorio completado

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/service/impl/MarcaServiceImp.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/service/impl/MarcaServiceImp.java
@@ -1,10 +1,14 @@
 package pe.edu.upeu.sysventas.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import pe.edu.upeu.sysventas.dto.ComboBoxOption;
 import pe.edu.upeu.sysventas.model.Marca;
 import pe.edu.upeu.sysventas.repository.ICrudGenericoRepository;
 import pe.edu.upeu.sysventas.repository.MarcaRepository;
 import pe.edu.upeu.sysventas.service.IMarcaService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class MarcaServiceImp extends CrudGenericoServiceImp<Marca, Long> implements IMarcaService {
@@ -14,4 +18,18 @@ public class MarcaServiceImp extends CrudGenericoServiceImp<Marca, Long> impleme
     protected ICrudGenericoRepository<Marca, Long> getRepo() {
         return marcaRepository;
     }
+
+    @Override
+    public List<ComboBoxOption> listarCombobox() {
+        List<ComboBoxOption> listar=new ArrayList<>();
+        ComboBoxOption cb;
+        for(Marca cate : marcaRepository.findAll()) {
+            cb=new ComboBoxOption();
+            cb.setKey(String.valueOf(cate.getIdMarca()));
+            cb.setValue(cate.getNombre());
+            listar.add(cb);
+        }
+        return listar;
+    }
+
 }
